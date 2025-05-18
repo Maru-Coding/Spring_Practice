6a50a44577b3292bd046395968ac53c79ee8ebd9
Maru <partnersoul12@gmail.com>
2025-05-18 12:46:29 +0900

    롬복 의존성 주입 및 샘플 소스 생성

---
+++ SpringPractice/src/main/java/com/myspring/sample/Chef.java
@@ -0,0 +1,11 @@
+package com.myspring.sample;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component // 스프링에게 해당 클래스가 스프링에서 관리해야 하는 대상
+@Data // getter, setter, toString 등 포함
+public class Chef {
+	
+}
