c02c0b392587d40a2fbab84e89bc712260a6a23e
Maru <partnersoul12@gmail.com>
2025-05-20 20:33:59 +0900

    MyBatis 테스트

--- SpringPractice/src/test/java/com/myspring/persistence/DataSourceTests.java
+++ SpringPractice/src/test/java/com/myspring/persistence/DataSourceTests.java
@@ -2,6 +2,9 @@ package com.myspring.persistence;
 
 import static org.junit.Assert.fail;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
 import java.sql.Connection;
 
 import javax.sql.DataSource;
@@ -21,6 +24,21 @@ import lombok.extern.log4j.Log4j;
 public class DataSourceTests {
 	@Setter(onMethod_ = { @Autowired })
 	private DataSource dataSource;
+	
+	@Setter(onMethod_ = { @Autowired })
+	private SqlSessionFactory sqlSessionFactory;
+	
+	@Test
+	public void testMyBatis() {
+		try (SqlSession session = sqlSessionFactory.openSession();
+				Connection con = session.getConnection();
+		) {
+			log.info(session);
+			log.info(con);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
 	
 	@Test
 	public void testConnection() {
