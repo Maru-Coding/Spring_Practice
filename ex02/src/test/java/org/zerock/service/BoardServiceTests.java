b7df3f65275e0254a82674f6b3569ae464aa0c3c
Maru <partnersoul12@gmail.com>
2025-05-27 20:07:22 +0900

    서비스 구현 및 테스트

---
+++ ex02/src/test/java/org/zerock/service/BoardServiceTests.java
@@ -0,0 +1,67 @@
+package org.zerock.service;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class BoardServiceTests {
+	@Setter(onMethod_= {@Autowired})
+	private BoardService service;
+	
+	@Test
+	public void testExist() {
+		log.info(service);
+		assertNotNull(service);
+	}
+	
+	@Test
+	public void testRegister() {
+		BoardVO board = new BoardVO();
+		board.setTitle("테스트 글1");
+		board.setContent("테스트!");
+		board.setWriter("마루");
+		
+		service.register(board);
+		
+		log.info("게시글 생성된 번호 : " + board.getBno());
+	}
+	
+	@Test
+	public void testGetList() {
+		service.getList().forEach(board -> log.info(board));
+	}
+	
+	@Test
+	public void testGet() {
+		log.info(service.get(1L));
+	}
+	
+	@Test
+	public void testDelete() {
+		// 게시물 번호의 존재 여부를 확인 후 테스트
+		log.info("REMOVE RESULT: " + service.remove(2L));
+	}
+	
+	@Test
+	public void testUpdate() {
+		BoardVO board = service.get(1L);
+		
+		if(board == null) {
+			return;
+		}
+		
+		board.setTitle("수정 테스트!");
+		log.info("MODIFY RESULT: " + service.modify(board));
+	}
+}
