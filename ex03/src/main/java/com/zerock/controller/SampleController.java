dea2fa9a6bc0daa39d7f63054f7fcac4c326a1eb
Maru <partnersoul12@gmail.com>
2025-06-16 22:12:45 +0900

    REST API HTTP 응답 실습

--- ex03/src/main/java/com/zerock/controller/SampleController.java
+++ ex03/src/main/java/com/zerock/controller/SampleController.java
@@ -6,7 +6,9 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +20,7 @@ import lombok.extern.log4j.Log4j;
 @RequestMapping("/sample")
 @Log4j
 public class SampleController {
+	// 텍스트 반환
 	@GetMapping(value = "/getText", produces = "text/plain; charset=UTF-8")
 	public String getText() {
 		log.info("MIME TYPE : " + MediaType.TEXT_PLAIN_VALUE);
@@ -25,13 +28,15 @@ public class SampleController {
 		return "안녕하세요";
 	}
 	
+	// VO 반환
 	@GetMapping(value = "/getSample",
 				produces = { MediaType.APPLICATION_JSON_UTF8_VALUE,
-							MediaType.APPLICATION_XML_VALUE})
+							MediaType.APPLICATION_XML_VALUE}) // JSON, XML 값을 보여주기 위한 Produce 설정
 	public SampleVO getSample() {
 		return new SampleVO(112, "스타", "로드");
 	}
 	
+	// 리스트 반환
 	@GetMapping(value = "/getList",
 				produces = { MediaType.APPLICATION_JSON_UTF8_VALUE,
 							MediaType.APPLICATION_XML_VALUE})
@@ -40,6 +45,7 @@ public class SampleController {
 				.collect(Collectors.toList());
 	}
 	
+	// 맵 반환
 	@GetMapping(value = "/getMap",
 				produces = { MediaType.APPLICATION_JSON_UTF8_VALUE,
 							MediaType.APPLICATION_XML_VALUE})
@@ -48,5 +54,23 @@ public class SampleController {
 		map.put("First", new SampleVO(111, "그루트", "주니어"));
 		
 		return map;
+	}
+	
+	// HTTP 응답 반환 엔티티
+	@GetMapping(value = "/check", params = {"height", "weight"})
+	public ResponseEntity<SampleVO> check(Double height, Double weight){
+		SampleVO vo = new SampleVO(0, "" + height, "" + weight);
+		
+		ResponseEntity<SampleVO> result = null;
+		
+		// 개발자 도구 - NetWork - Name에서 Headers 정보 확인
+		// http://localhost:8080/controller/sample/check?height=150&weight=50
+		if(height < 150) {
+			result = ResponseEntity.status(HttpStatus.BAD_GATEWAY).body(vo);
+		} else {
+			result = ResponseEntity.status(HttpStatus.OK).body(vo);
+		}
+		
+		return result;
 	}
 }
