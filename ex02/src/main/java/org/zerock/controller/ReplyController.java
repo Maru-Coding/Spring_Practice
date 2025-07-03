9f55b2e01942cfc5af74c3765b8d38dc10f735dd
Maru <partnersoul12@gmail.com>
2025-07-03 21:54:21 +0900

    댓글 조회 기능 구현

--- ex02/src/main/java/org/zerock/controller/ReplyController.java
+++ ex02/src/main/java/org/zerock/controller/ReplyController.java
@@ -1,12 +1,17 @@
 package org.zerock.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerock.domain.Criteria;
 import org.zerock.domain.ReplyVO;
 import org.zerock.service.ReplyService;
 
@@ -20,6 +25,7 @@ import lombok.extern.log4j.Log4j;
 public class ReplyController {
 	private ReplyService service;
 	
+	// 댓글 등록
 	@PostMapping(value = "/new",
 			consumes = "application/json",
 			produces = {MediaType.TEXT_PLAIN_VALUE })
@@ -34,4 +40,24 @@ public class ReplyController {
 				? new ResponseEntity<>("success", HttpStatus.OK)
 						: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
+	
+	// 댓글 조회
+	@GetMapping(value = "/pages/{bno}/{page}",
+			produces = {
+					MediaType.APPLICATION_XML_VALUE,
+					MediaType.APPLICATION_JSON_UTF8_VALUE })
+	public ResponseEntity<List<ReplyVO>> getList(
+			@PathVariable("page") int page,
+			@PathVariable("bno") Long bno){
+		
+		log.info("getList......");
+		
+		Criteria cri = new Criteria(page, 10);
+		
+		log.info(cri);
+		
+		return new ResponseEntity<>(service.getList(cri, bno), HttpStatus.OK);
+		
+	}
+	
 }
