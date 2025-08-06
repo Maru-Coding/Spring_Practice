92035d164a85d8747dc153ccfddeb1d3635eec55
Maru <partnersoul12@gmail.com>
2025-08-06 21:41:03 +0900

    댓글 페이징 처리 JSON 데이터 반환 구현

--- ex02/src/main/java/org/zerock/controller/ReplyController.java
+++ ex02/src/main/java/org/zerock/controller/ReplyController.java
@@ -14,11 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.domain.Criteria;
+import org.zerock.domain.ReplyPageDTO;
 import org.zerock.domain.ReplyVO;
 import org.zerock.service.ReplyService;
 
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j;
 
 @RequestMapping("/replies")
@@ -44,6 +44,7 @@ public class ReplyController {
 						: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
+	/*
 	// 댓글 조회(전체)
 	@GetMapping(value = "/pages/{bno}/{page}",
 			produces = {
@@ -61,6 +62,22 @@ public class ReplyController {
 		
 		return new ResponseEntity<>(service.getList(cri, bno), HttpStatus.OK);
 		
+	}
+	*/
+	
+	
+	// 댓글 페이징처리 후 조회
+	@GetMapping(value = "/pages/{bno}/{page}",
+			produces = {
+					MediaType.APPLICATION_XML_VALUE,
+					MediaType.APPLICATION_JSON_UTF8_VALUE })
+	public ResponseEntity<ReplyPageDTO> getList(@PathVariable("page") int page, @PathVariable("bno") Long bno){
+		Criteria cri = new Criteria(page, 10);
+		
+		log.info("get Reply List bno: " + bno);
+		log.info("cri: " + cri);
+		
+		return new ResponseEntity<>(service.getListPage(cri, bno), HttpStatus.OK);
 	}
 	
 	// 댓글 조회(1건)
